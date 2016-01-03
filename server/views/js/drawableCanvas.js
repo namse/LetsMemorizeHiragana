00fb7b000b1d2a794829d277f616f78bbe144b7f
Nam Se Hyun <skatpgusskat@naver.com>
2016-01-03 12:20:35 +0000

    drawableCanvas with eventListener

---
+++ server/views/js/drawableCanvas.js
@@ -0,0 +1,89 @@
+/* global EventListener */
+var DrawableCanvas = function(jqueryCanvasObject, jqueryCanvasContainerObject){
+    EventListener.call(this);    
+    
+    
+    var jqueryCanvasObject = jqueryCanvasObject;
+    var jqueryCanvasContainerObject = jqueryCanvasContainerObject;
+    var domCanvas = jqueryCanvasObject[0];
+    var canvasCTX = domCanvas.getContext("2d");
+    var self = this;
+    var clicked = false;
+    
+    this.strokeStyle = "#000";
+    this.lineWidth = "5";
+    
+    this.clear = function() {
+        canvasCTX.clearRect(0, 0, jqueryCanvasObject.width(), jqueryCanvasObject.height());
+    };
+    this.resizeCanvas = function() {
+        domCanvas.width = jqueryCanvasContainerObject.width();
+        domCanvas.height = jqueryCanvasContainerObject.height();
+    };
+    
+    
+    // event
+    // 'strokeStart', 'strokeMove'
+    
+    function strokeStart(pageX, pageY){
+        canvasCTX.beginPath();
+        var x = pageX - jqueryCanvasObject.offset().left;
+        var y = pageY - jqueryCanvasObject.offset().top;
+        canvasCTX.strokeStyle = self.strokeStyle;
+        canvasCTX.lineWidth = self.lineWidth;
+        canvasCTX.moveTo(x, y);
+        self.fireEvent('strokeStart', [x, y]);
+    }
+    
+    function strokeMove(pageX, pageY){
+        var x = pageX - jqueryCanvasObject.offset().left;
+        var y = pageY - jqueryCanvasObject.offset().top;
+        canvasCTX.strokeStyle = self.strokeStyle;
+        canvasCTX.lineWidth = self.lineWidth;
+        canvasCTX.moveTo(x, y);
+        canvasCTX.stroke();
+        self.fireEvent('strokeMove', [x, y]);
+    }
+    
+    
+    
+    jqueryCanvasObject.on("touchstart", function(e){
+        e = e.originalEvent;
+        self.strokeStart(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+    });
+    
+    jqueryCanvasObject.on("touchmove", function(e) {
+        e.preventDefault();
+        e = e.originalEvent;
+        self.strokeMove(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+    });
+    
+    jqueryCanvasObject.on("MSPointerDown", function(e){
+        e = e.originalEvent;
+        self.strokeStart(e.pageX, e.pageY);
+    });
+    
+    jqueryCanvasObject.on("MSPointerMove", function(e){
+        e = e.originalEvent;
+        e = e.originalEvent;
+        self.strokeMove(e.pageX, e.pageY);
+    });
+    
+    
+    
+    jqueryCanvasObject.on("mousedown", function(e){
+        self.clicked = true;
+        self.strokeMove(e.pageX, e.pageY);
+    });
+    
+    
+    jqueryCanvasObject.on("mousemove", function(e){
+        if(self.clicked){
+            self.strokeMove(e.pageX, e.pageY);
+        }
+    });
+    $(window).on("mouseup", function(e){
+        self.clicked = false;
+    });
+    
+}
---
+++ server/views/js/eventListener.js
@@ -0,0 +1,38 @@
+var EventListener = function(){
+    var events = {};
+
+    function on(name, handler) {
+        if (events.hasOwnProperty(name))
+            events[name].push(handler);
+        else
+            events[name] = [handler];
+    };
+    
+    function addEventListener(name, handler) {
+        this.on(name, handler);
+    };
+
+    function removeEventListener(name, handler) {
+        /* This is a bit tricky, because how would you identify functions?
+           This simple solution should work if you pass THE SAME handler. */
+        if (!events.hasOwnProperty(name))
+            return;
+
+        var index = events[name].indexOf(handler);
+        if (index != -1)
+            events[name].splice(index, 1);
+    };
+
+    function fireEvent(name, args) {
+        if (!events.hasOwnProperty(name))
+            return;
+
+        if (!args || !args.length)
+            args = [];
+
+        var evs = events[name], l = evs.length;
+        for (var i = 0; i < l; i++) {
+            evs[i].apply(null, args);
+        }
+    };
+}
